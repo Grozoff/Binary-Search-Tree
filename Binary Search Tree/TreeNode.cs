9b85e399a58e0543088a03061ba0196ef32ae6d8
Sergey <grozoff92@gmail.com>
2021-06-07 09:43:35 +0500

    Add project files.

---
+++ Binary Search Tree/TreeNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Binary_Search_Tree
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
